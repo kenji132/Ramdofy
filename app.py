f0aed96dda661be18fc58b09d5587ad5b7567acb
kenji132 <ue52483@gmail.com>
2021-08-11 23:59:24 +0900

    flashメッセージ追加

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import datetime
 import spotipy
 import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
-from flask import Flask, flash, redirect, render_template, request, jsonify
+from flask import Flask, flash, redirect, render_template, request, jsonify,session
 
+app = Flask(__name__)
 
 if not os.environ.get("SPOTIPY_USER_NAME_ID"):
     raise RuntimeError("SPOTIPY_USER_NAME_ID not set")
@@ -14,10 +15,13 @@ if not os.environ.get("SPOTIPY_CLIENT_SECRET"):
     raise RuntimeError("SPOTIPY_CLIENT_SECRET not set")
 if not os.environ.get("SPOTIPY_REDIRECT_URI"):
     raise RuntimeError("SPOTIPY_REDIRECT_URI not set")
+if not os.environ.get("SECRET_KEY"):
+    raise RuntimeError("SECRET_KEY not set")
 username = os.environ.get("SPOTIPY_USER_NAME_ID")
 client_id = os.environ.get("SPOTIPY_CLIENT_ID")
 client_secret = os.environ.get("SPOTIPY_CLIENT_SECRET")
 redirect_uri = os.environ.get("SPOTIPY_REDIRECT_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 scope = 'user-library-read user-read-playback-state playlist-read-private user-read-recently-played playlist-read-collaborative playlist-modify-public playlist-modify-private user-follow-modify user-follow-read'
@@ -46,36 +50,47 @@ take_next_artists(artist)
 client_credentials_manager = spotipy.oauth2.SpotifyClientCredentials(client_id, client_secret)
 
 
-app = Flask(__name__)
-
 @app.route('/', methods = ['GET'])
 def index():
   return render_template("index.html", followed_artists = artist["artists"]["items"])
 
 
-@app.route('/relax')
+@app.route('/relax', methods=['GET', 'POST'])
 def relax():
-  now = datetime.date.today().strftime("%y%y/%m/%d")
-  new_playlist = spotify.user_playlist_create(user = username, name = now + "relax")
-  url_list = []
-  for track in saved_tracks["items"]:
-    feature = spotify.audio_features(track["track"]["id"])
-    if feature[0]["loudness"] <= -10 and feature[0]["acousticness"] >= 0.5 and feature[0]["instrumentalness"] >= 0.8:
-      url_list.append(track["track"]["external_urls"]["spotify"])
-  spotify.user_playlist_add_tracks(username, new_playlist["id"], url_list)
-  return redirect("/")
+  if request.method == 'POST':
+    now = datetime.date.today().strftime("%y%y/%m/%d")
+    url_list = []
+    for track in saved_tracks["items"]:
+      feature = spotify.audio_features(track["track"]["id"])
+      if feature[0]["loudness"] <= -10 and feature[0]["acousticness"] >= 0.5 and feature[0]["instrumentalness"] >= 0.8:
+        url_list.append(track["track"]["external_urls"]["spotify"])
+    if len(url_list) == 0:
+      flash("No tracks found")
+      return render_template("relax.html")
+    new_playlist = spotify.user_playlist_create(user = username, name = now + "relax")
+    spotify.user_playlist_add_tracks(username, new_playlist["id"], url_list)
+    return redirect(new_playlist["external_urls"]["spotify"])
+  else:
+    return render_template("relax.html")
 
-@app.route('/workout')
+@app.route('/workout', methods=['GET', 'POST'])
 def workout():
-  now = datetime.date.today().strftime("%y%y/%m/%d")
-  new_playlist = spotify.user_playlist_create(user = username, name = now + "workout")
-  url_list = []
-  for track in saved_tracks["items"]:
-    feature = spotify.audio_features(track["track"]["id"])
-    if feature[0]["tempo"] >= 160 and feature[0]["acousticness"] <= 0.01 and feature[0]["energy"] >= 0.9:
-      url_list.append(track["track"]["external_urls"]["spotify"])
-  spotify.user_playlist_add_tracks(username, new_playlist["id"], url_list)
-  return redirect(new_playlist["external_urls"]["spotify"])
+  if request.method == 'POST':
+    now = datetime.date.today().strftime("%y%y/%m/%d")
+    tempo = request.form.get('tempo')
+    url_list = []
+    for track in saved_tracks["items"]:
+      feature = spotify.audio_features(track["track"]["id"])
+      if (feature[0]["tempo"] >= (float(tempo) - 5) and feature[0]["tempo"] <= (float(tempo) + 5)) and feature[0]["acousticness"] <= 0.01 and feature[0]["energy"] >= 0.9:
+        url_list.append(track["track"]["external_urls"]["spotify"])
+    if len(url_list) == 0:
+      flash("No tracks found")
+      return render_template("workout.html")
+    new_playlist = spotify.user_playlist_create(user = username, name = now + "workout")
+    spotify.user_playlist_add_tracks(username, new_playlist["id"], url_list)
+    return redirect(new_playlist["external_urls"]["spotify"])
+  else:
+    return render_template("workout.html")
 
 @app.route('/favorite', methods=['GET', 'POST'])
 def favorite():
